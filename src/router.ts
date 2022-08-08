fe208e07ba0a05e26eb6c7d9662dfb316b33c524
Ary <arylmoraesn@gmail.com>
2022-08-08 16:31:19 -0600

    domlima

--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,16 @@
-import express from 'express';
-import nodemailer from 'nodemailer';
+import express from "express";
+import nodemailer from "nodemailer";
 
 const router = express.Router();
 
-router.post('/mail', async (req, res) => {
+router.post("/mail", async (req, res) => {
   const transporter = nodemailer.createTransport({
     auth: {
       pass: process.env.SMTP_PASSWORD,
       user: process.env.SMTP_EMAIL,
     },
-    host: 'smtp.zoho.com',
-    port: 465,
+    host: process.env.SMTP_HOST || "smtp.zoho.com",
+    port: +process.env.SMTP_PORT || 465,
     secure: true, // use SSL
   });
 
@@ -18,28 +18,28 @@ router.post('/mail', async (req, res) => {
 
   const userData = `
     Dados do usuario:\n
-    Nome: ${name || ''}\n
-    Email: ${email || ''}\n
-    Telefone: ${phone || ''}\n
-    Cidade: ${city || ''}\n`;
+    Nome: ${name || ""}\n
+    Email: ${email || ""}\n
+    Telefone: ${phone || ""}\n
+    Cidade: ${city || ""}\n`;
 
   const emailText = `${userData}\nMensagem:\n ${message}`;
 
   const mailOptions = {
     from: process.env.SMTP_EMAIL,
-    subject: subject || 'Sem titulo',
-    text: emailText || 'Email vazio, culpe danlimax',
+    subject: subject || "Sem titulo",
+    text: emailText || "Email vazio, culpe danlimax",
     to: process.env.SMTP_EMAIL,
   };
 
   transporter.sendMail(mailOptions, function (error) {
     if (error) {
       res.send({
-        error
+        error,
       });
     } else {
       res.send({
-        message: 'Email enviado com sucesso',
+        message: "Email enviado com sucesso",
       });
     }
   });
